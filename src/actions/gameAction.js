560188c906184690b54431a4c2f72083265c7c29
Anishb7 <anishbhattaraicsit2014@gmail.com>
2020-11-22 17:53:30 +0545

    4.  Fetching other Games

--- src/actions/gameAction.js
+++ src/actions/gameAction.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
-import { popularGameURL } from '../api';
+import { popularGameURL, upcomingGamesURL, newGamesURL } from '../api';
 
 //Action Creator
 
 export const loadGames = () => async (dispatch) => {
   //Fetch Axios
   const popularData = await axios.get(popularGameURL());
+  const upcomingData = await axios.get(upcomingGamesURL());
+  const newGamesData = await axios.get(newGamesURL());
   dispatch({
     type: 'FETCH_GAMES',
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
